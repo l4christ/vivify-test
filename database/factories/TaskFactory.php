65336b6a918815c31010bb69c3a68f14bffa5085
Emmanuel <ademolaemmanuel2@yahoo.com>
2023-10-16 00:58:28 +0100

    Define logic in TaskFactory for generating random task data

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class TaskFactory extends Factory
 {
+    protected $model = Task::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,9 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'status' => $this->faker->randomElement(['todo', 'done', 'inprogress']),
         ];
     }
 }
