96f046488ad082b705c4619c39c8c60d95f1fb7e
Emmanuel <ademolaemmanuel2@yahoo.com>
2023-10-15 22:26:20 +0100

    Create 'Tasks' section and button to the task-management view

--- resources/views/livewire/task-management.blade.php
+++ resources/views/livewire/task-management.blade.php
@@ -1,3 +1,12 @@
 <div>
-    Testing the Livewire Component
+    <div class="container mx-auto px-4 sm:px-8 py-8">
+
+        <div class="flex justify-between">
+            <h2 class="text-2xl font-semibold leading-tight">Tasks</h2>
+            <button 
+                class="bg-purple-600 px-4 py-2 rounded-md text-white hover:bg-purple-500 cursor-pointer">
+                Add new Task
+            </button>
+        </div>
+    </div>
 </div>
