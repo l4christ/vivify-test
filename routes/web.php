11dd3c540946d929a97884a1d218fa9c0b253bf0
Emmanuel <ademolaemmanuel2@yahoo.com>
2023-10-16 02:19:59 +0100

    Update route to redirect to breeze signup page

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect()->route('register');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
