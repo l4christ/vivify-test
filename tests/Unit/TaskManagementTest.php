106735ea370bf0d97ad77eb97e2f7dedca665825
Emmanuel <ademolaemmanuel2@yahoo.com>
2023-10-16 01:10:55 +0100

    Add unit tests to validate  the 'title', 'description' and status fields are required

--- tests/Unit/TaskManagementTest.php
+++ tests/Unit/TaskManagementTest.php
@@ -2,15 +2,37 @@
 
 namespace Tests\Unit;
 
+use Livewire\Livewire;
 use PHPUnit\Framework\TestCase;
+use App\Livewire\TaskManagement;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskManagementTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    
+    public function test_title_field_is_required_and_valid()
     {
-        $this->assertTrue(true);
+        Livewire::test(TaskManagement::class)
+            ->set('title', '')
+            ->call('createNewTask')
+            ->assertHasErrors('title');
+    }
+    
+    public function test_description_field_is_required_and_valid()
+    {
+        Livewire::test(TaskManagement::class)
+            ->set('description', '')
+            ->call('createNewTask')
+            ->assertHasErrors('description');
+    }
+    
+    public function test_status_field_is_required_and_valid()
+    {
+        Livewire::test(TaskManagement::class)
+            ->set('status', '')
+            ->call('createNewTask')
+            ->assertHasErrors('status');
     }
 }
