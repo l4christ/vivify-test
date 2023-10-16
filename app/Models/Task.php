e81f5616b00aca5af957236194c5dd98265842d1
Emmanuel <ademolaemmanuel2@yahoo.com>
2023-10-16 02:43:53 +0100

    Implement accessors for title and description fields

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,16 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
     use HasFactory;
+
 
     protected $fillable = [
         'title',
         'description',
         'status'
     ];
+
+    public function getTitleExcerptAttribute()
+    {
+        $limit = 30; 
+        $title = $this->attributes['title'];
+
+        if (strlen($title) > $limit) {
+            return substr($title, 0, $limit) . '...';
+        }
+
+        return $title;
+    }
+
+    public function getDescriptionExcerptAttribute()
+    {
+        $limit = 50; 
+        $description = $this->attributes['description'];
+
+        if (strlen($description) > $limit) {
+            return substr($description, 0, $limit) . '...';
+        }
+
+        return $description;
+    }
 }
--- resources/views/livewire/task-management.blade.php
+++ resources/views/livewire/task-management.blade.php
@@ -61,13 +61,13 @@
                                 <div class="flex items-center">
                                     <div class="ml-3">
                                         <p class="text-gray-900 whitespace-no-wrap">
-                                            {{ $task->title }}
+                                            {{ $task->title_excerpt }}
                                         </p>
                                     </div>
                                 </div>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">{{ $task->description }}</p>
+                                <p class="text-gray-900 whitespace-no-wrap">{{ $task->description_excerpt }}</p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <p class="text-gray-900 whitespace-no-wrap">
